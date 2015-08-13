c1575beb658c2f15a4e5202a2b32d1d8ed52a206
mashoedoe <murraycraib@gmail.com>
2015-08-13 15:48:27 +0200

    hover examples added

---
+++ examples/app5.R
@@ -0,0 +1,189 @@
+library(shiny)
+library(shinythemes)
+library(leaflet)
+library(DT)
+library(rgdal)
+
+wards_SA_ordered <- readRDS("RDS/wards_SA_ordered.rds")
+wards_SA_ordered@data$DENSITY <- round(wards_SA_ordered@data$DENSITY, digits = 1)
+
+ui <- navbarPage(
+  title = "Night Light & Population Density",
+  tabPanel(
+    title = "Map", 
+    leafletOutput(outputId = "map", width = "100%", height = "700px"),
+    absolutePanel(top = 70, left = 70,
+      h3("Manipulating a population density choropleth map of South Africa to match
+         satelite imagery of Night Light",
+         style="color:white"),
+      p("Experiment with different upper threshold values to alter how the 
+        population density choropleth displays.",
+        br(),
+        "The default setting is to include the 70% least densely populated wards in the choropleth palette.",
+        br(),
+        "The remaining 30% most densely populated wards are coded as the color 'white' by default.",
+        br(),
+        "See how changing these defaults with the slider and dropdown, changes the map display",
+         style="color:white"),
+      sliderInput(inputId = "integer", 
+                  label = span(HTML("Density Threshold in People/km<sup>2</sup>"), style="color:white"),  
+                  min = min(wards_SA_ordered@data$DENSITY),
+                  max = max(wards_SA_ordered@data$DENSITY), 
+                  round = -1, 
+                  step = 1,
+                  value = round(quantile(x = wards_SA_ordered@data$DENSITY, 
+                                         probs = 0.7), 
+                                digits = 0),
+                  width = "850px"
+      ),
+      selectInput(
+        inputId = "select_densities", 
+        label = span("Density Threshold in % of Wards",style="color:white"),
+        choices = trunc(quantile(x = wards_SA_ordered@data$DENSITY, 
+                                 probs = seq(0, 1, 0.05)
+                                 ),
+                        digits = 1), 
+        selected = trunc(quantile(x = wards_SA_ordered@data$DENSITY, 
+                                  probs = 0.7),digits = 1)
+        ),
+      checkboxInput(
+        inputId = "ward_polygons", 
+        label = span("Show Population Density Polygons over Satelite Imagery", style="color:white"), TRUE
+      )
+                  
+    )
+  ),
+  
+  tabPanel(
+    title = "Documentation",
+    wellPanel(style = "color: #09183e;",
+      includeMarkdown("documentation.md")
+    )
+  ),
+  
+  tabPanel(
+    title = "Ward Data",
+    wellPanel(style = "color: #09183e;",
+              datatable(wards_SA_ordered@data)
+    )
+  )
+)
+
+  
+                 
+server <- function(input, output, session) {
+  #   # Reactive expression for the data subsetted to what the user selected
+  #   filteredData <- reactive({
+  #     wards_SA_ordered[wards_SA_ordered@data$DENSITY >= 0 & wards_SA_ordered@data$DENSITY <= input$integer,]
+  #   })
+  
+  # This reactive expression represents the palette function,
+  # which changes as the user makes selections in UI.
+  colorpal <- reactive({
+    colorNumeric(c(marray::maPalette(low = "#09183e", mid = "yellow", high = "white",
+                                     k = dim(wards_SA_ordered[wards_SA_ordered@data$DENSITY <= input$integer,])[1]),
+                   rep(x = "#FFFFFF", 
+                       times = (4277 - dim(wards_SA_ordered[wards_SA_ordered@data$DENSITY <= input$integer,])[1]))),
+                 domain = c(0, input$integer), na.color = "white")
+  })
+  
+  
+  output$map <- renderLeaflet({
+    # Use leaflet() here, and only include aspects of the map that
+    # won't need to change dynamically (at least, not unless the
+    # entire map is being torn down and recreated).
+    leaflet() %>% 
+      addTiles(
+        urlTemplate <- "http://map1.vis.earthdata.nasa.gov/wmts-webmerc/VIIRS_CityLights_2012/default/GoogleMapsCompatible_Level8/{z}/{y}/{x}.jpeg", 
+        group = "Night Light (default)",
+        attribution = paste('Imagery provided by GIBS,',
+                            'operated by the NASA/GSFC/<a href="https://earthdata.nasa.gov">EOSDIS</a>',
+                            'with funding provided by NASA/HQ.')
+      ) %>%
+      setView(
+        lng = 25, lat = -28, zoom = 6
+        )
+  })
+  
+  # control the slider to match the dropdown when the user chooses the dropdown 
+  # rather than the slider
+  observe({
+    val <- input$select_densities
+    updateSliderInput(session, "integer", value = val)
+  })
+  
+  # Incremental changes to the map (in this case, changing the chorpleth palette and 
+  # coloring of the wards when a new threshold population density is chosen) should 
+  # be performed in an observer. Each independent set of things that can change
+  # should be managed in its own observer.
+  observe({
+    proxy1 <- leafletProxy("map", data = wards_SA_ordered) 
+    
+    proxy1 %>% clearShapes()
+    pal <- colorpal()
+    if (input$ward_polygons) {proxy1 %>%
+        addPolygons(stroke = T, weight = 1, opacity = 0, color = "white", fillOpacity = 1,
+                    smoothFactor = 0.5, fillColor = ~pal(DENSITY) #,
+#                     popup = ~paste0(wards_SA_ordered@data$MUNICNAME,
+#                                     "<br />",
+#                                     "<strong> Ward </strong>",
+#                                     wards_SA_ordered@data$WARDNO,
+#                                     "<br />",
+#                                     "<strong>Density: </strong>",
+#                                     round(wards_SA_ordered@data$DENSITY, digits = 1),
+#                                     " people/km",
+#                                     "<sup>2</sup>"),
+#                     options = popupOptions()
+        )
+    }    
+  })
+
+  observe({
+    proxy3 <- leafletProxy("map", data = wards_SA_ordered) 
+    
+    proxy3 %>% clearPopups()
+    if (input$ward_polygons) {proxy1 %>%
+        addPopups(
+          data = wards_SA_ordered,
+          popup = ~paste0(wards_SA_ordered@data$MUNICNAME,
+                          "<br />",
+                          "<strong> Ward </strong>",
+                          wards_SA_ordered@data$WARDNO,
+                          "<br />",
+                          "<strong>Density: </strong>",
+                          round(wards_SA_ordered@data$DENSITY, digits = 1),
+                          " people/km",
+                          "<sup>2</sup>"),
+          options = popupOptions()
+        )
+        
+        )
+    }    
+  })  
+
+  # Use a separate observer to recreate the legend as needed.
+  observe({
+    proxy2 <- leafletProxy("map", data = wards_SA_ordered)
+    
+    # Remove any existing legend, and only if the legend is
+    # enabled, create a new one.
+    proxy2 %>% clearControls()
+    pal <- colorpal()
+    proxy2 %>% addLegend(position = "bottomright", pal = pal, values = ~DENSITY,
+                         title = "Population/km<sup>2</sup>", opacity = 1, na.label = "Over 1500", 
+                         labFormat = labelFormat(big.mark = " ")
+                         
+    )
+    
+  })
+}
+
+shinyApp(ui, server) 
+
+
+
+
+
+  
+          
+          
---
+++ examples/hover example shp.R
@@ -0,0 +1,284 @@
+# https://github.com/rstudio/leaflet/blob/master/inst/examples/shiny.R
+library(shiny)
+library(leaflet)
+#options(unzip = "internal"); devtools::install_github("ropensci/geojsonio")
+library(geojsonio)
+library(spatstat)
+library(rgdal)
+
+#town_SA <- readRDS("/media/mashudu/A296E14C96E12191/Copy/git/Light_vs_Population/RDS/town_SA.rds")
+#geodata <- paste(readLines(system.file("examples/test.json", package = "leaflet")), collapse = "\n")
+town_tj <- paste(readLines("/media/mashudu/A296E14C96E12191/Copy/git/Light_vs_Population/TopoJSON/LocalMunicipalities2011.json"), collapse = "\n")
+#town_json <- jsonlite::validate(town_tj)
+#town_json <- jsonlite::fromJSON(town_tj)
+#geo_json <- jsonlite::fromJSON(geodata)
+town_shp <- readOGR(dsn = "/media/mashudu/A296E14C96E12191/Copy/git/Light_vs_Population/SHP/LocalMunicipalities2011.shp", 
+                            layer = "LocalMunicipalities2011")
+#geodata_sp <- geojson_read(system.file("examples/test.json", package = "leaflet"))
+#file.copy(from = "/media/mashudu/A296E14C96E12191/Copy/git/Light_vs_Population/TopoJSON/LocalMunicipalities2011.json",
+#          to = "/media/mashudu/A296E14C96E12191/Copy/git/Light_vs_Population/TopoJSON/LocalMunicipalities2011.topojson")
+#town_tj_sp1 <- topojson_read(x="/media/mashudu/A296E14C96E12191/Copy/git/Light_vs_Population/TopoJSON/LocalMunicipalities2011.topojson")
+#town_tj_sp <- readOGR(dsn = "/media/mashudu/A296E14C96E12191/Copy/git/Light_vs_Population/TopoJSON/LocalMunicipalities2011.json", 
+#                      layer = "LocalMunicipalities2011")
+town_shp_gj <- geojson_list(town_shp)
+
+# replace geojson with town_shp_gj
+# town_json$type
+# 
+# geo_json$type
+# head(town_json$objects$LocalMunicipalities2011$type)
+# 
+# geo_json$features$geometry$type
+# head(town_json$objects$LocalMunicipalities2011$geometries$type)
+# 
+# geo_json$features$geometry$coordinates
+# 
+# geo_json$features$properties
+# town_json$objects$LocalMunicipalities2011$geometries$properties
+# 
+# town_json$objects$LocalMunicipalities2011$geometries$properties$CAT_B
+#   
+# geo_json$features$type
+# town_json$objects$LocalMunicipalities2011$geometries$type
+# 
+# geo_json$features
+# town_json$objects$LocalMunicipalities2011$geometries
+# 
+# 
+# head(town_json$objects$LocalMunicipalities2011$geometries$properties$CAT_B)
+
+
+ui_shp <- fluidPage(
+  leafletOutput("map1"),
+#  checkboxInput("addMarker", "Add marker on click"),
+#  actionButton("clearMarkers", "Clear all markers"),
+  textOutput("message1", container = h6),
+  textOutput("message2", container = h6),
+  textOutput("message3", container = h6),
+  textOutput("message4", container = h6),
+  textOutput("message5", container = h6),
+  textOutput("message6", container = h6),
+  textOutput("message7", container = h6)
+)
+
+
+server_shp <- function(input, output, session) {
+  v <- reactiveValues(msg1 = "")
+  v <- reactiveValues(msg2 = "")
+  v <- reactiveValues(msg3 = "")
+  v <- reactiveValues(msg4 = "")
+  v <- reactiveValues(msg5 = "")
+  v <- reactiveValues(msg6 = "")
+  v <- reactiveValues(msg7 = "")
+  
+  output$map1 <- renderLeaflet({
+      leaflet() %>%
+      addGeoJSON(geojson = town_shp_gj) %>%
+#          addPolygons(data = town_shp,
+#                      popup = paste(town_shp@data$MUNICNAME,
+#                                    "<br>",
+#                                    "Size:",
+#                                    town_shp@data$AREA,
+#                                    " km<sup>2</sup>")
+#                      ) %>%
+      #      addCircles(-60, 60, radius = 5e5, layerId = "circle") %>%
+      setView(zoom = 5, lng = 27, lat = -27) # %>%
+      #fitBounds(lng1 = -87.1875, lat1 = 71.4131, lng2 = 128.3203, lat2 = 0.3515)
+#       addPopups(data = t(sapply(slot(town_shp, 'polygons'), function(i) slot(i, 'labpt'))) , popup = paste(town_shp@data$MUNICNAME,
+#                               "<br>",
+#                               "Size:",
+#                               town_shp@data$AREA,
+#                               " km<sup>2</sup>"))
+  })
+  
+  observeEvent(input$map1_geojson_mouseover, {
+    v$msg1 <- paste("Mouse is over", input$map1_geojson_mouseover$featureId)
+      })
+  observeEvent(input$map1_geojson_mouseout, {
+    v$msg1 <- ""
+  })
+   observeEvent(input$map1_geojson_click, {
+     v$msg2 <- paste("Clicked on", input$map1_geojson_click$featureId)
+        })
+#  observeEvent(input$map1_shape_mouseover, {
+#    v$msg3 <- paste("Mouse is over shape", input$map1_shape_mouseover$id)
+#  })
+#  observeEvent(input$map1_shape_mouseout, {
+#    v$msg3 <- ""
+#  })
+#  observeEvent(input$map1_shape_click, {
+#    v$msg4 <- paste("Clicked shape", input$map1_shape_click$id)
+#  })
+  observeEvent(input$map1_click, {
+    v$msg5 <- paste("Mouse coordinates: Lat", round(input$map1_click$lat, digits = 3), 
+                    "Long", round(input$map1_click$lng, digits = 3))
+#     if (input$addMarker) {
+#       leafletProxy("map1") %>%
+#         addMarkers(lng = input$map1_click$lng, lat = input$map1_click$lat)
+#     }
+  })
+  observeEvent(input$map1_geojson_mouseover, {
+    v$msg5 <- paste("Mouse coordinates: Lat", round(input$map1_popup_mouseover$lat, digits = 3), 
+                    "Long", round(input$map1_polygon_mouseover$lng, digits = 3))
+  })
+  observeEvent(input$map1_geojson_mouseout, {
+    v$msg5 <- ""
+  })
+  
+  #  observeEvent(input$map1_mouseout, {
+#    v$msg5 <- ""
+#  })
+  
+  observeEvent(input$map1_zoom, {
+    v$msg6 <- paste("Zoom is", input$map1_zoom)
+  })
+  observeEvent(input$map1_bounds, {
+    v$msg7 <- paste("Bounds: lat",
+                    substr(paste(input$map1_bounds[1]),start = 1, stop = 6),"long",
+                    substr(paste(input$map1_bounds[4]),start = 1, stop = 6),"(topleft); lat", 
+                    substr(paste(input$map1_bounds[3]),start = 1, stop = 6),"long", 
+                    substr(paste(input$map1_bounds[2]),start = 1, stop = 6),"(bottomright)")
+      
+#      "Bounds: lat", (input$map1_bounds[1]),"long", round(input$map1_bounds[2],3), "(topright);")#,
+#                    "lat", round(input$map1_bounds[3],3), "long", round(input$map1_bounds[4],3), "(bottomleft)") #, collapse = ", "
+  })
+#   observeEvent(input$clearMarkers, {
+#     leafletProxy("map1") %>% clearMarkers()
+#   })
+  
+  output$message1 <- renderText(v$msg1)
+  output$message2 <- renderText(v$msg2)
+  output$message3 <- renderText(v$msg3)
+  output$message4 <- renderText(v$msg4)
+  output$message5 <- renderText(v$msg5)
+  output$message6 <- renderText(v$msg6)
+  output$message7 <- renderText(v$msg7)
+}
+
+shinyApp(ui_shp, server_shp)
+
+# library(shiny)
+# library(leaflet)
+# 
+# geodata <- paste(readLines(system.file("examples/test.json", package = "leaflet")), collapse = "\n")
+# 
+# ui <- fluidPage(
+#   leafletOutput("map1"),
+#   checkboxInput("addMarker", "Add marker on click"),
+#   actionButton("clearMarkers", "Clear all markers"),
+#   textOutput("message", container = h3)
+# )
+# 
+# server <- function(input, output, session) {
+#   v <- reactiveValues(msg = "")
+#   
+#   output$map1 <- renderLeaflet({
+#     leaflet() %>%
+#       addGeoJSON(geodata) %>%
+#       addCircles(-60, 60, radius = 5e5, layerId = "circle") %>%
+#       fitBounds(-87.1875, 71.4131, 128.3203, 0.3515)
+#   })
+#   
+#   observeEvent(input$map1_geojson_mouseover, {
+#     v$msg <- paste("Mouse is over", input$map1_geojson_mouseover$featureId)
+#   })
+#   observeEvent(input$map1_geojson_mouseout, {
+#     v$msg <- ""
+#   })
+#   observeEvent(input$map1_geojson_click, {
+#     v$msg <- paste("Clicked on", input$map1_geojson_click$featureId)
+#   })
+#   observeEvent(input$map1_shape_mouseover, {
+#     v$msg <- paste("Mouse is over shape", input$map1_shape_mouseover$id)
+#   })
+#   observeEvent(input$map1_shape_mouseout, {
+#     v$msg <- ""
+#   })
+#   observeEvent(input$map1_shape_click, {
+#     v$msg <- paste("Clicked shape", input$map1_shape_click$id)
+#   })
+#   observeEvent(input$map1_click, {
+#     v$msg <- paste("Clicked map at", input$map1_click$lat, "/", input$map1_click$lng)
+#     if (input$addMarker) {
+#       leafletProxy("map1") %>%
+#         addMarkers(lng = input$map1_click$lng, lat = input$map1_click$lat)
+#     }
+#   })
+#   observeEvent(input$map1_zoom, {
+#     v$msg <- paste("Zoom changed to", input$map1_zoom)
+#   })
+#   observeEvent(input$map1_bounds, {
+#     v$msg <- paste("Bounds changed to", paste(input$map1_bounds, collapse = ", "))
+#   })
+#   observeEvent(input$clearMarkers, {
+#     leafletProxy("map1") %>% clearMarkers()
+#   })
+#   
+#   output$message <- renderText(v$msg)
+# }
+# 
+# shinyApp(ui, server)
+# 
+# 
+# library(shiny)
+# library(leaflet)
+# 
+# geodata <- paste(readLines(system.file("examples/test.json", package = "leaflet")), collapse = "\n")
+# 
+# ui <- fluidPage(
+#   leafletOutput("map1"),
+#   checkboxInput("addMarker", "Add marker on click"),
+#   actionButton("clearMarkers", "Clear all markers"),
+#   textOutput("message", container = h3)
+# )
+# 
+# server <- function(input, output, session) {
+#   v <- reactiveValues(msg = "")
+#   
+#   output$map1 <- renderLeaflet({
+#     leaflet() %>%
+#       addGeoJSON(geodata) %>%
+#       addCircles(-60, 60, radius = 5e5, layerId = "circle") %>%
+#       fitBounds(-87.1875, 71.4131, 128.3203, 0.3515)
+#   })
+#   
+#   observeEvent(input$map1_geojson_mouseover, {
+#     v$msg <- paste("Mouse is over", input$map1_geojson_mouseover$featureId)
+#   })
+#   observeEvent(input$map1_geojson_mouseout, {
+#     v$msg <- ""
+#   })
+#   observeEvent(input$map1_geojson_click, {
+#     v$msg <- paste("Clicked on", input$map1_geojson_click$featureId)
+#   })
+#   observeEvent(input$map1_shape_mouseover, {
+#     v$msg <- paste("Mouse is over shape", input$map1_shape_mouseover$id)
+#   })
+#   observeEvent(input$map1_shape_mouseout, {
+#     v$msg <- ""
+#   })
+#   observeEvent(input$map1_shape_click, {
+#     v$msg <- paste("Clicked shape", input$map1_shape_click$id)
+#   })
+#   observeEvent(input$map1_click, {
+#     v$msg <- paste("Clicked map at", input$map1_click$lat, "/", input$map1_click$lng)
+#     if (input$addMarker) {
+#       leafletProxy("map1") %>%
+#         addMarkers(lng = input$map1_click$lng, lat = input$map1_click$lat)
+#     }
+#   })
+#   observeEvent(input$map1_zoom, {
+#     v$msg <- paste("Zoom changed to", input$map1_zoom)
+#   })
+#   observeEvent(input$map1_bounds, {
+#     v$msg <- paste("Bounds changed to", paste(input$map1_bounds, collapse = ", "))
+#   })
+#   observeEvent(input$clearMarkers, {
+#     leafletProxy("map1") %>% clearMarkers()
+#   })
+#   
+#   output$message <- renderText(v$msg)
+# }
+# 
+# shinyApp(ui, server)
+
